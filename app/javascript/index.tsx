6c63a8da1214c79b40cb5425c20f6b08e843cfe1
tanishi <tanishi503@gmail.com>
2020-05-30 23:43:33 +0900

    feat: initialize for rails

---
+++ app/javascript/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<p>Hello World!</p>, document.getElementById("app"));
+});
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+const { NODE_ENV } = process.env;
+const isProd = NODE_ENV === "production";
+
+module.exports = {
+  mode: isProd ? "production" : "development",
+  devtool: "source-map",
+  entry: {
+    application: path.resolve(__dirname, "app/javascript/index.tsx")
+  },
+  output: {
+    path: path.resolve(__dirname, "public/packs"),
+    publicPath: isProd ? "/packs/" : "//localhost:8081/packs/",
+    filename: isProd ? "[name]-[hash].js" : "[name].js"
+  },
+  resolve: {
+    extensions: [".js", "jsx", "ts", "tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/packs/",
+    host: "localhost",
+    port: 8081,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  plugins: [new WebpackAssetsManifest({ publicPath: true, writeToDisk: true })]
+};
